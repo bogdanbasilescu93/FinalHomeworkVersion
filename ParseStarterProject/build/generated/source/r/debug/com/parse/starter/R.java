56c4ed5d19a3f7cb399972d033250a3bd9ec42fb
bogdanbasilescu93 <b.bogdan0993@yahoo.com>
2015-01-16 23:04:45 +0200

    latest update - documentation

--- ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
+++ ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
@@ -10,50 +10,54 @@ package com.parse.starter;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int login=0x7f020001;
         public static final int splash_photo=0x7f020002;
     }
     public static final class id {
-        public static final int add=0x7f060006;
-        public static final int button=0x7f060021;
-        public static final int button2=0x7f060022;
-        public static final int button3=0x7f060020;
-        public static final int button4=0x7f060023;
-        public static final int button5=0x7f060025;
-        public static final int button6=0x7f060001;
-        public static final int button7=0x7f06001b;
-        public static final int checkBox1=0x7f06000b;
-        public static final int checkBox2=0x7f06000c;
-        public static final int checkBox3=0x7f06000d;
-        public static final int editText=0x7f060003;
-        public static final int editText2=0x7f060002;
-        public static final int editText3=0x7f060017;
-        public static final int editText4=0x7f060019;
-        public static final int etpassword=0x7f060013;
-        public static final int etusername=0x7f060012;
-        public static final int hiddenLayout=0x7f060009;
-        public static final int linearLayout1=0x7f060005;
-        public static final int linearLayout2=0x7f060008;
-        public static final int listView=0x7f06001a;
-        public static final int login=0x7f060014;
-        public static final int logout=0x7f06001f;
-        public static final int radio0=0x7f06000f;
-        public static final int radio1=0x7f060010;
-        public static final int radio2=0x7f060011;
-        public static final int radioGroup1=0x7f06000e;
-        public static final int remove=0x7f060007;
-        public static final int signup=0x7f060015;
-        public static final int textView=0x7f060024;
-        public static final int textView1=0x7f06000a;
-        public static final int textView2=0x7f060000;
-        public static final int textView3=0x7f060004;
-        public static final int textView4=0x7f060016;
-        public static final int textView5=0x7f060018;
-        public static final int textView6=0x7f06001c;
-        public static final int txtuser=0x7f06001e;
-        public static final int txtwelcome=0x7f06001d;
+        public static final int add=0x7f070006;
+        public static final int button=0x7f070021;
+        public static final int button1=0x7f07001c;
+        public static final int button2=0x7f070022;
+        public static final int button3=0x7f070020;
+        public static final int button4=0x7f070023;
+        public static final int button5=0x7f070025;
+        public static final int button6=0x7f070001;
+        public static final int checkBox1=0x7f07000b;
+        public static final int checkBox2=0x7f07000c;
+        public static final int checkBox3=0x7f07000d;
+        public static final int editText=0x7f070003;
+        public static final int editText1=0x7f070016;
+        public static final int editText2=0x7f070002;
+        public static final int editText3=0x7f070019;
+        public static final int editText4=0x7f07001a;
+        public static final int editText5=0x7f07001b;
+        public static final int etpassword=0x7f070013;
+        public static final int etusername=0x7f070012;
+        public static final int hiddenLayout=0x7f070009;
+        public static final int linearLayout1=0x7f070005;
+        public static final int linearLayout2=0x7f070008;
+        public static final int login=0x7f070014;
+        public static final int logout=0x7f07001f;
+        public static final int radio0=0x7f07000f;
+        public static final int radio1=0x7f070010;
+        public static final int radio2=0x7f070011;
+        public static final int radioGroup1=0x7f07000e;
+        public static final int remove=0x7f070007;
+        public static final int signup=0x7f070015;
+        public static final int textView=0x7f070024;
+        public static final int textView1=0x7f07000a;
+        public static final int textView2=0x7f070000;
+        public static final int textView3=0x7f070004;
+        public static final int textView4=0x7f070017;
+        public static final int textView5=0x7f070018;
+        public static final int txtuser=0x7f07001e;
+        public static final int txtwelcome=0x7f07001d;
     }
     public static final class layout {
         public static final int changepass=0x7f030000;
@@ -69,23 +73,31 @@ public final class R {
         public static final int splash_sound=0x7f040001;
     }
     public static final class string {
-        public static final int AddView=0x7f050000;
-        public static final int ChangePassAct=0x7f050001;
-        public static final int ChangePassword=0x7f050002;
-        public static final int CheckWeather=0x7f050003;
-        public static final int City=0x7f050004;
-        public static final int Login=0x7f050005;
-        public static final int LogoutBtn=0x7f050006;
-        public static final int Music=0x7f050007;
-        public static final int RemoveView=0x7f050008;
-        public static final int SignUp=0x7f050009;
-        public static final int UsefulLinks=0x7f05000a;
-        public static final int Welcome=0x7f05000b;
-        public static final int app_name=0x7f05000c;
-        public static final int hello=0x7f05000d;
-        public static final int hint1=0x7f05000e;
-        public static final int hint2=0x7f05000f;
-        public static final int outputWeather=0x7f050010;
-        public static final int pass1=0x7f050011;
+        public static final int AddView=0x7f060000;
+        public static final int ChangePassAct=0x7f060001;
+        public static final int ChangePassword=0x7f060002;
+        public static final int CheckWeather=0x7f060003;
+        public static final int City=0x7f060004;
+        public static final int Login=0x7f060005;
+        public static final int LogoutBtn=0x7f060006;
+        public static final int Music=0x7f060007;
+        public static final int RemoveView=0x7f060008;
+        public static final int SignUp=0x7f060009;
+        public static final int UsefulLinks=0x7f06000a;
+        public static final int Welcome=0x7f06000b;
+        public static final int action_settings=0x7f06000c;
+        public static final int app_name=0x7f06000d;
+        public static final int country=0x7f06000e;
+        public static final int hello=0x7f06000f;
+        public static final int hello_world=0x7f060010;
+        public static final int hint1=0x7f060011;
+        public static final int hint2=0x7f060012;
+        public static final int humidity=0x7f060013;
+        public static final int location=0x7f060014;
+        public static final int outputWeather=0x7f060015;
+        public static final int pass1=0x7f060016;
+        public static final int pressure=0x7f060017;
+        public static final int temperature=0x7f060018;
+        public static final int weather=0x7f060019;
     }
 }
